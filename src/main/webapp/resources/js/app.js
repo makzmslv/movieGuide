4214178d28ab01fda7df7cb8dac39a90e1da61d5
Makarand Salvi <makarand.salvi@neptune-int.com>
2014-03-15 01:07:10 +0530

    Implemented basic movie list page

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -4,13 +4,16 @@ var pricesApp = angular.module('movieGuide', [ 'ngRoute', 'movieGuideControllers
 
 // Declare app level module which depends on filters, and services
 pricesApp.config([ '$routeProvider', function($routeProvider) {
-	$routeProvider.when('/movies', {
-		templateUrl : 'resources/html/search.html',
+	$routeProvider.when('/list', {
+		templateUrl : 'resources/html/list.html',
 		controller : 'MovieController'
 	}).when('/add', {
         templateUrl: 'resources/html/add.html',
         controller: 'addController'
-      });
+      }).when('/showDetails', {
+          templateUrl: 'resources/html/showDetails.html',
+          controller: 'addController'
+        });
 
 	$routeProvider.otherwise({
 		redirectTo : '/'
--- src/main/webapp/resources/js/controllers.js
+++ src/main/webapp/resources/js/controllers.js
@@ -1,11 +1,15 @@
 
 var movieGuideControllers = angular.module('movieGuideControllers', []);
 
-movieGuideControllers.controller('MovieController', ['$scope', 'Movie',function($scope, Movie)
+movieGuideControllers.controller('MovieController', ['$scope','$location', 'Movie',function($scope, $location, Movie)
  {
 	$scope.hello = "hello";
 	 $scope.movies = Movie.query();
-	 $scope.movie = Movie.get({},{'movieName' : 'Inception'});
+
+	 $scope.showDetails = function(index)
+	 {
+		 $location.url("/showDetails");
+	 };
  }]);
 
 
